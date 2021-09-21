ea06350a75d0f517ce85c0c99e069670de02dc64
Mehedi Hasan <mehedi@augnitive.com>
2021-09-21 15:18:00 +0600

    Day-9, Profile Screen

---
+++ lib/profile.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage("assets/poor_man.png"),
+            ),
+            Container(
+              child: Text(
+                "Mr. Mehedi Hasan",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontFamily: "Lobster",
+                  fontWeight: FontWeight.w700
+                ),
+              ),
+            ),
+            Text(
+              "Flutter Developer",
+              style: TextStyle(
+                fontSize:24,
+                fontFamily: "SourceCodePro",
+                fontWeight: FontWeight.w500,
+                letterSpacing: 2.5,
+                color: Colors.white.withOpacity(0.75) 
+              ),
+            ),
+            Divider(
+              thickness: 1,
+              color: Colors.white.withOpacity(0.50),
+              endIndent: 130,
+              indent: 130,
+              height: 40,
+            ),
+            Container(
+              width: double.infinity,
+              height: 48,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5)
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "+01482145215",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 24
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 48,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5)
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 28,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "mehedi@augnitive.com",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 24
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

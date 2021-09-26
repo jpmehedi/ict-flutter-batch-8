39689c8bf310498f048af0e339fd0df2519bac26
Mehedi Hasan <mehedi@augnitive.com>
2021-09-26 15:14:13 +0600

    Stack, Positioned, Drawer

--- lib/contact.dart
+++ lib/contact.dart
@@ -6,15 +6,77 @@ class ContactScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ListView & ListTile"),),
-      body: ListView.separated(
-        separatorBuilder: (BuildContext context, int index){
-          return Divider(
-            thickness: 1,
-            color: Colors.teal,
-            height: 0,
-          );
-        },
+      appBar: AppBar(
+        // leading: Icon(Icons.menu),
+        title: Text("ListView & ListTile"),
+      ),
+
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset("assets/background.jpg"),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: CircleAvatar(
+                    radius: 32,
+                    backgroundImage: AssetImage("assets/poor_man.png"),
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 80,
+                  child: Text(
+                    "Mehedi Hasan",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 110,
+                  child: Text(
+                    "mehedihasaninfrom@gmail.com",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      body: ListView.builder( 
+        // separatorBuilder: (BuildContext context, int index){
+        //   return Divider(
+        //     thickness: 1,
+        //     color: Colors.teal,
+      
+
+        //     height: 0,
+        //   );
+        // }, 
         itemCount: 60,
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
@@ -29,6 +91,7 @@ class ContactScreen extends StatelessWidget {
           );
         },
       )
+
     );
   }
 }

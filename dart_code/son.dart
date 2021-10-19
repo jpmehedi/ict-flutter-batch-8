cbee7c3a1d241339f921be1ac5423d565c9a3fc3
Mehedi Hasan <mehedi@augnitive.com>
2021-10-19 17:16:45 +0600

    OOP

---
+++ dart_code/son.dart
@@ -0,0 +1,28 @@
+import 'father.dart';
+
+class Son extends Father {
+  String? name;
+  int? age;
+  Son({this.name, this.age});
+
+  int _a = 30;
+  int b = 200;
+
+  namePrint() {
+    return name;
+  }
+  _multiplication() {
+    return _a * b;
+  }
+
+  @override
+  xyz() {
+    // print("Hello bangladesh");
+    int a = 20;
+    int b = 30;
+    print(a + b);
+    return super.xyz();
+  }
+
+
+}

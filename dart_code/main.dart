1559918b14ccd4febc4f71d24f39a3e2aa811a29
Mehedi Hasan <mehedi@augnitive.com>
2021-10-24 15:44:48 +0600

    Try catch,async await, mixin, final const

--- dart_code/main.dart
+++ dart_code/main.dart
@@ -1,4 +1,4 @@
-import 'son.dart';
+
 
 void main () {
 
@@ -11,12 +11,45 @@ void main () {
   // print(Father.addTwo());
 
 
-  Son son = Son( age: 25,name: "Hello",);
+  print("Before async");
+  runForFiveSecond();
+  print("After async");
+
+
+
+
+  // Son son = Son( age: 25,name: "Hello",);
+
+  // son.substraction();
 
   // print(son.)
   // print(son.addTwo());
   // print(son.multiplication());
-  print(son.namePrint());
+  // print(son.namePrint());
   
 
+      
+//   final String name;
+  
+//   name = "Hasan";
+  
+//   final DateTime dateTime = DateTime.now();
+  
+  //  const DateTime dateTime = DateTime.now();
+  
+  
+//   const int age = 85;
+  
+//   age = 18;
+  
+  
+  
+  // print(dateTime);
+
 }
+
+
+ Future runForFiveSecond()async{
+    await Future.delayed(Duration(seconds: 5));
+    print("Procece complete");
+  }
---
+++ dart_code/mother.dart
@@ -0,0 +1,11 @@
+mixin Mother{
+
+substraction() {
+
+  int a = 30;
+  int b = 4;
+
+  print(a - b);
+}
+
+}

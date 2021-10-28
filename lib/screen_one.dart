9eceea8ff9ea351c614b23727ad7dd5aae1a8d5f
Mehedi Hasan <mehedi@augnitive.com>
2021-10-28 14:52:41 +0600

    Name route

--- lib/screen_one.dart
+++ lib/screen_one.dart
@@ -2,6 +2,7 @@ import 'package:demo_project/screen_two.dart';
 import 'package:flutter/material.dart';
 
 class ScreenOne extends StatelessWidget {
+   static final String path = "ScreenOne";
   const ScreenOne({ Key? key }) : super(key: key);
 
   @override
@@ -13,14 +14,7 @@ class ScreenOne extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> ScreenTwo()));
-            
-            // Route route = MaterialPageRoute(builder: (context)=> ScreenTwo());
-            // Navigator.push(context, route);
-
-            // Navigator.push(context, MaterialPageRoute(builder: (context){
-            //  return ScreenTwo();
-            // }));
+            Navigator.pushNamed(context, ScreenTwo.path);
 
           }, 
           child: Text("Screen One")
